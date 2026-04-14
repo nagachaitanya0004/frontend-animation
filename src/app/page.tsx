c7b35b614f70b9f0e212320a22ad5958b0509513
Viyyapu Naga Chaitanya <nagachaitanyaviyyapu@gmail.com>
2026-04-14 10:26:35 +0530

    initial setup with next app and basic cleanup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      minHeight: '100vh',
+      flexDirection: 'column'
+    }}>
+      <h1>Cloud Optimization Insights</h1>
+    </main>
+  );
+}
